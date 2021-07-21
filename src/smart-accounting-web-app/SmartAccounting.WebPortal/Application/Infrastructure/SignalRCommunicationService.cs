fccc388a0e8d9239683df34f6f59f5ce7ac387c8
Daniel-Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2021-07-21 07:48:46 +0200

    Update SignalRCommunicationService.cs

--- src/smart-accounting-web-app/SmartAccounting.WebPortal/Application/Infrastructure/SignalRCommunicationService.cs
+++ src/smart-accounting-web-app/SmartAccounting.WebPortal/Application/Infrastructure/SignalRCommunicationService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Web;
 using SmartAccounting.WebPortal.Application.Model;
+using System;
 using System.Threading.Tasks;
 
 namespace SmartAccounting.WebPortal.Application.Infrastructure
@@ -12,27 +13,62 @@ namespace SmartAccounting.WebPortal.Application.Infrastructure
         private readonly IConfiguration _config;
         private HubConnection _hubConnection;
 
-        public async Task InitializeSignalRConnectionAsync()
+        public event Action<DocumentProcessedNotification> OnMessageReceived;
+
+        public SignalRCommunicationService(ITokenAcquisition tokenAcquisition,
+                                           IConfiguration config)
         {
-            var hubUrl = _config["SmartAccountingApis:NotificationHubUrl"];
+            _tokenAcquisition = tokenAcquisition ?? throw new ArgumentNullException(nameof(tokenAcquisition));
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+        }
 
-            _hubConnection = new HubConnectionBuilder()
-                .WithUrl(hubUrl, options =>
-                {
-                    options.AccessTokenProvider = async () =>
+        public async Task InitializeAsync()
+        {
+            try
+            {
+                var hubUrl = _config["SmartAccountingApis:NotificationHubUrl"];
+
+                _hubConnection = new HubConnectionBuilder()
+                    .WithUrl(hubUrl, options =>
                     {
-                        string[] scopes = new[] { _config["SmartAccountingApis:NotificationApiScope"] };
-                        string accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(scopes);
-                        return accessToken;
-                    };
-                })
-                .Build();
-            await _hubConnection.StartAsync();
+                        options.AccessTokenProvider = async () =>
+                        {
+                            string[] scopes = new[] { _config["SmartAccountingApis:NotificationApiScope"] };
+                            string accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+                            return accessToken;
+                        };
+                    })
+                    .Build();
 
+                await _hubConnection.StartAsync();
+            }
+
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("IDW10502",
+                                       StringComparison.InvariantCultureIgnoreCase))
+                {
+                    System.Diagnostics.Debug.WriteLine("SignalR connection cannot be estabilished before user is authenticated.");
+                }
+
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("SignalR connection cannot be estabilished - unauthorized access.");
+                }
+            }
+        }
+
+        public void SubscribeHubMethod()
+        {
             _hubConnection.On<DocumentProcessedNotification>("direct-notification", (documentProcessedNotification) =>
             {
-                // Handle message...
+                OnMessageReceived?.Invoke(documentProcessedNotification);
             });
+        }
+
+        public async Task CloseConnectionAsync()
+        {
+            await _hubConnection.DisposeAsync();
         }
     }
 }
